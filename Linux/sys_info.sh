d427ce1964c60607876138fd141e26f33a00a3fd
RamRod-0 <jrrod327@gmail.com>
2020-08-26 21:05:09 -0700

    First commit

--- Linux/sys_info.sh
+++ Linux/sys_info.sh
@@ -1,37 +1,50 @@
 #!/bin/bash
 
-output=$HOME/research/sys_info.txt
-ip=$(ip addr | grep inet | head -1 | tail -2)
-suids=$(find / -type -perm /4000)
+#Check if script was run as root. Exit if true.
+if [ $UID -eq 0 ]
+then
+  echo "Please do not run this script as root."
+  exit
+fi
 
-#check if directory exists, create if not
+# Define Variables
+output=$HOME/research/sys_info.txt
+ip=$(ip addr | grep inet | tail -2 | head -1)
+suids=$(sudo find / -type f -perm /4000 2> /dev/null)
+
+
+# Check for research directory. Create it if needed.
 if [ ! -d $HOME/research ]
 then
-	mkdir $HOME/research
+ mkdir $HOME/research
 fi
 
-#remove old file
+# Check for output file. Clear it if needed.
 if [ -f $output ]
 then
-	rm $output
+  rm $output
 fi
 
-
-
-echo "A Quick Systems Audit Script" >> $output
+echo "A Quick System Audit Script" >> $output
 date >> $output
 echo "" >> $output
 echo "Machine Type Info:" >> $output
-echo $MACHTYPE >> $output
+echo -e "$MACHTYPE \n" >> $output
 echo -e "Uname info: $(uname -a) \n" >> $output
+echo -e "IP Info:" >> $output
 echo -e "$ip \n" >> $output
-echo "Hostname: $(hostname -s) " >> $output
+echo -e "Hostname: $(hostname -s) \n" >> $output
 echo "DNS Servers: " >> $output
 cat /etc/resolv.conf >> $output
-echo "Memory Info: " >> $output
+echo -e "\nMemory Info:" >> $output
 free >> $output
-echo -e "\nCPU Info: " >> $output
+echo -e "\nCPU Info:" >> $output
 lscpu | grep CPU >> $output
-echo -e "\mDisk Usage:" >> $output
-echo -e "\nWho is logged in: \n $(who) \n" >> $output
+echo -e "\nDisk Usage:" >> $output
+df -H | head -2 >> $output
+echo -e "\nWho is logged in: \n $(who -a) \n" >> $output
+echo -e "\nSUID Files:" >> $output
 echo $suids >> $output
+echo -e "\nTop 10 Processes" >> $output
+ps aux --sort -%mem | awk {'print $1, $2, $3, $4, $11'} | head >> $output
+
